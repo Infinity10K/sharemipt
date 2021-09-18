2a35c266d7d5b276df233068f5cadaa2ad238685
Salokhiddin Rakhmonkulov <infinity10k@ya.ru>
2021-09-18 14:02:08 +0300

    quadratic equation

---
+++ 1_quadratic_eq.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+void findRoots(double a, double b, double c){
+    if (a == 0)
+        printf("It is not quadratic equation!");
+        
+    else {
+        float D = b * b - 4 * a * c;
+        float aa = 2 * a;
+        
+        if (D == 0)
+            printf("x1 = x2 = %.3f", b / aa);
+            
+        else if (D > 0) {
+            float sD = sqrt(D);
+            float x1 = (-b + sD) / aa;
+            float x2 = (-b - sD) / aa;
+            
+            printf("x1 = %.3f\nx2 = %.3f", x1, x2);
+        }
+
+        else {
+            printf("No real roots");
+        }
+    }
+    printf("\n\n");
+}
+
+
+int main(){
+    float a, b, c;
+    
+    /*
+    printf("Input a, b, c: ");
+    scanf("%f %f %f", &a, &b, &c);
+    
+    findRoots(a, b, c);
+    */
+
+    findRoots(0, 1, 2);
+    findRoots(1, 2, 1);
+    findRoots(1, 2, 3);
+    findRoots(1, 3, 2);
+    findRoots(.15, .41, .187);
+    findRoots(1, 0, 0);
+
+    return 0;
+}
